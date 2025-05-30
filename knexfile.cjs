3ae485977e8b9b7411e41d6037284fff293ad758
Pinyaev Vladimir <avallakhh@gmail.com>
2025-05-30 22:16:45 +0300

    Step 4. Render on Railway

---
+++ knexfile.cjs
@@ -0,0 +1,32 @@
+const path = require('path');
+
+const migrations = {
+  directory: path.join(__dirname, 'server', 'migrations'),
+};
+
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, 'database.sqlite'),
+    },
+    useNullAsDefault: true,
+    migrations,
+  },
+
+  test: {
+    client: 'sqlite3',
+    connection: ':memory:',
+    useNullAsDefault: true,
+    migrations,
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
+    migrations,
+  },
+};
